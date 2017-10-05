b6f497971a4086af1b5bf3708d530446f06794ea
Jere Jones <jere.jones@gmail.com>
2017-10-05 16:21:08 -0400

    Fix test failure due to refactoring

--- namers_test.go
+++ namers_test.go
@@ -3,7 +3,7 @@ package jlog
 import "testing"
 
 func TestCurrentPackageFileName(t *testing.T) {
-	expected := "github.com/jerejones/jlog/global_test.go"
+	expected := "github.com/jerejones/jlog/namers_test.go"
 	actual := CurrentPackageFileName()
 	if expected != actual {
 		t.Errorf("Expected: %s\nGot: %s", expected, actual)
